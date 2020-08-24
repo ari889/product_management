462bdbe6132c012e40ed888dea1b40e05c477717
ari889 <arijitbanarjee889@gmail.com>
2020-08-24 12:27:34 +0600

    Product Show

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+	require_once 'app/autoload.php';
+
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +13,53 @@
 </head>
 <body>
 
+
+	<?php
+
+	if(isset($_POST['padd'])){
+
+		//value get
+		$pname = $_POST['pname'];
+		$pprice = $_POST['pprice'];
+		$pquan= $_POST['pquan'];
+
+		$total = NULL;
+		if(!empty($pprice) && !empty($pquan)){
+				$total = $pprice * $pquan;
+		}
+
+
+		//product photo upload
+		$file = fileUpload($_FILES['pphoto'], 'products/');
+
+		$photo_name = $file['file_name'];
+
+
+		//product upload
+		if(empty($pname) || empty($pprice) || empty($pquan) || empty($total)){
+			$mess = '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+			 				  <strong>Warning!</strong> All fileds are required.
+			 				  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			 				    <span aria-hidden="true">&times;</span>
+			 				  </button>
+			 				</div>';
+		}else if(!empty($file['mess'])){
+			$mess = $file['mess'];
+		}else{
+			$sql = "INSERT INTO products(photo, product_name, product_price, quantity, total) VALUES('$photo_name', '$pname', '$pprice', '$pquan', '$total')";
+			$connection -> query($sql);
+			$mess = '<div class="alert alert-success alert-dismissible fade show" role="alert">
+			 				  <strong>Success!</strong> Product upload successfully.
+			 				  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			 				    <span aria-hidden="true">&times;</span>
+			 				  </button>
+			 				</div>';
+		}
+
+	}
+
+	 ?>
+
 	<div class="container mt-5">
     <div class="card w-50 mb-3">
     	<div class="card-header">
@@ -15,24 +67,29 @@
     	</div>
 			<div class="card-body">
 				<form action="" method="POST" enctype="multipart/form-data">
+					<?php
+						if(isset($mess)){
+							echo $mess;
+						}
+					 ?>
 					<div class="form-group">
 						<label for="product-name">Product Name</label>
-						<input type="text" class="form-control" placeholder="Peroduct name">
+						<input name="pname" type="text" class="form-control" placeholder="Peroduct name">
 					</div>
 					<div class="form-group">
 						<label for="product-name">Product Price</label>
-						<input type="text" class="form-control" placeholder="Peroduct price">
+						<input name="pprice" type="text" class="form-control" placeholder="Peroduct price">
 					</div>
 					<div class="form-group">
 						<label for="product-name">Product Quantity</label>
-						<input type="text" class="form-control" placeholder="Peroduct quantity">
+						<input name="pquan" type="text" class="form-control" placeholder="Peroduct quantity">
 					</div>
 					<div class="form-group">
 						<label for="product-name">Product photo</label>
-						<input type="file" class="form-control">
+						<input name="pphoto" type="file" class="form-control">
 					</div>
 					<div class="form-group">
-						<input type="submit" class="btn btn-info" value="Add product">
+						<input name="padd" type="submit" class="btn btn-info" value="Add product">
 					</div>
 				</form>
 			</div>
@@ -55,19 +112,29 @@
             </tr>
           </thead>
           <tbody>
+
+						<?php
+
+						$sql = "SELECT * FROM products ORDER BY id DESC";
+						$all_products = $connection -> query($sql);
+						$i = 1;
+						while($product = $all_products -> fetch_assoc()):
+						 ?>
               <tr>
-                <th scope="row">1</th>
-                <td><img src="images/image.jpg" alt="" class="img-fluid custom-image"></td>
-                <td>Alu</td>
-                <td>220</td>
-                <td>10</td>
-                <td>2200</td>
+                <th scope="row"><?php echo $i; $i++; ?></th>
+                <td><img src="products/<?php echo $product['photo']; ?>" alt="" class="img-fluid custom-image"></td>
+                <td><?php echo $product['product_name']; ?></td>
+                <td><?php echo $product['product_price']; ?></td>
+                <td><?php echo $product['quantity']; ?></td>
+                <td><?php echo $product['total']; ?></td>
                 <td class="text-right">
                   <a href="show.php" class="btn btn-info">View</a>
                   <a href="#" class="btn btn-warning">Edit</a>
                   <a href="all-data.php" class="btn btn-danger" id="delete-btn">Delete</a>
                 </td>
               </tr>
+
+						<?php endwhile; ?>
 							<tr>
 								<td colspan="5" class="text-right">Total = </td>
 								<td>12000</td>
